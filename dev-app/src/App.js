e9debfc5e3f05f3d4ef1a56fb6444877ce759fda
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-07-18 14:37:10 +1200

    creating the landing page of the website

--- dev-app/src/App.js
+++ dev-app/src/App.js
@@ -5,20 +5,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-body">
+
+      {/*  /!*<p>*!/*/}
+      {/*  /!*  Edit <code>src/App.js</code> and save to reload.*!/*/}
+      {/*  /!*</p>*!/*/}
+      {/*  /!*<a*!/*/}
+      {/*  /!*  className="App-link"*!/*/}
+      {/*  /!*  href="https://reactjs.org"*!/*/}
+      {/*  /!*  target="_blank"*!/*/}
+      {/*  /!*  rel="noopener noreferrer"*!/*/}
+      {/*  /!*>*!/*/}
+      {/*  /!*  Learn React*!/*/}
+      {/*  /!*</a>*!/*/}
+      {/*  anything*/}
+          <div className="appIcon" >
+              <i className="fas fa-hourglass fa-2x"/>
+          </div>
+      </div>
     </div>
   );
 }
