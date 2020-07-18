629cfa6cda86cbea6349fcac9220b0c854155cd7
kabilank6 <kabilan-k@hotmail.com>
2020-07-18 21:23:11 +1200

    added state again to breaktime

--- src/components/Breaktime.js
+++ src/components/Breaktime.js
@@ -1,5 +1,46 @@
 import React from "react";
+
 class Breaktime extends React.Component {
+    state = {
+        break: "Whenever",
+        hours: "However long"
+    }
+	render() {
+		return (
+			<form>
+				<h3>How often do you want to go on break?</h3>
+				<div>
+                    <select 
+                        value={this.state.selectValue} 
+                        onChange={this.handleChange} 
+                    >
+                    <option value="Orange">Orange</option>
+                        <option value="Radish">Radish</option>
+                        <option value="Cherry">Cherry</option>
+                    </select>
+                    <p>{message}</p>
+                </div>        
+				<h3>Up to how long do you wish to work per day in total?</h3>
+				<div class="dropdown">
+					<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						{this.state.hours}
+					</button>
+					<div class="dropdown-menu" aria-labelledby="dropdownMenuButton" onChange = {this.handleChange}>
+						<a class="dropdown-item" id = "7 hours">7 hours</a>
+						<a class="dropdown-item" id ="8 hours">8 hours</a>
+						<a class="dropdown-item" id = "9 hours">9 hours</a>
+						<a class="dropdown-item" id = "10 hours">10 hours</a>
+						<a class="dropdown-item" id = "11 hours">11 hours</a>
+						<a class="dropdown-item" id = "12 hours">12 hours</a>
+					</div>
+				</div>
+			</form>
+
+		);
+	}
+}
+
+/* class Breaktime extends React.Component {
 	render() {
 		return (
 			<form>
@@ -36,7 +77,7 @@ class Breaktime extends React.Component {
 
 		);
 	}
-}
+} */
 
 
 /* class Breaktime extends React.Component {
