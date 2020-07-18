6db682f3c02d805ba2f77e411096a01311289a8b
cecilia Yin <ceciliayin@yahoo.com>
2020-07-18 17:55:50 +1200

    Changed the size of the app window

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -7,13 +7,11 @@ class Settings extends React.Component {
 				<nav>
 					<div className="nav nav-tabs" id="nav-tab" role="tablist">
 						<a className="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home"
-						   role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+						   role="tab" aria-controls="nav-home" aria-selected="true">Profile</a>
 						<a className="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
 						   role="tab" aria-controls="nav-profile" aria-selected="false">Break Time</a>
 						<a className="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
-						   role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-						<a className="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact"
-						   role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+						   role="tab" aria-controls="nav-profile" aria-selected="false">Groups</a>
 					</div>
 				</nav>
 
