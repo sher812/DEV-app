dc55fac3b24b1af09c96ac583615a2fefd40af27
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-07-18 18:02:02 +1200

    fix the problem in settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -6,22 +6,21 @@ class Settings extends React.Component {
 			<>
 				<nav>
 					<div className="nav nav-tabs" id="nav-tab" role="tablist">
-						<a className="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home"
+						<a className="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-profile"
 						   role="tab" aria-controls="nav-home" aria-selected="true">Profile</a>
-						<a className="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
+						<a className="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-breakTime"
 						   role="tab" aria-controls="nav-profile" aria-selected="false">Break Time</a>
-						<a className="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
+						<a className="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-group"
 						   role="tab" aria-controls="nav-profile" aria-selected="false">Groups</a>
 					</div>
 				</nav>
 
 				<div className="tab-content" id="nav-tabContent">
-					<div className="tab-pane fade show active" id="nav-home" role="tabpanel"
+					<div className="tab-pane fade show active" id="nav-profile" role="tabpanel"
 						 aria-labelledby="nav-home-tab">...
 					</div>
-					<div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">break time</div>
-					<div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
-					<div className="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+					<div className="tab-pane fade" id="nav-breakTime" role="tabpanel" aria-labelledby="nav-profile-tab">break time</div>
+					<div className="tab-pane fade" id="nav-group" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
 				</div>
 			</>
 		);
