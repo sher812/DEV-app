1a28513953d8d69cf5927bbc3ea844fef691d408
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-07-19 11:27:26 +1200

    align texts in profile and groups

--- src/components/Group.js
+++ src/components/Group.js
@@ -5,23 +5,62 @@ import Profilepic from "./profilepic.png";
 class Group extends React.Component {
 	render() {
 		return (
-			<>
-				
+
                 <div className="tab-content" id="nav-tabContent">
 					<div className="tab-pane fade show active" id="nav-group" role="tabpanel"
 						 aria-labelledby="nav-home-tab">
                         <img src={Profilepic} alt="profile" className = "profilepic" />
                         <div className="settingText">
                         <text>Members: {"\n"}</text> <br/>
-                        <text> <span className="tab">Jane</span>    Total points:xxxx</text> <br/>
-                        <text> <span className="tab">Jake</span>   Total points:xxxx</text> <br/>
-                        <text> <span className="tab">Bob</span>    Total points:xxxx</text> <br/>
-                        <text> <span className="tab">Rachel</span> Total points:xxxx</text> <br/>
-                        <text> <span className="tab">Huang</span>  Total points:xxxx</text> <br/>
+
+							<div className="row justify-content-center">
+								<div className="col-4 tab">
+									Jane
+								</div>
+								<div className="col-4" style={{ textAlign: 'left' }}>
+									Total points:xxxx
+								</div>
+							</div>
+
+							<div className="row justify-content-center">
+								<div className="col-4 tab">
+									Jake
+								</div>
+								<div className="col-4" style={{ textAlign: 'left' }}>
+									Total points:xxxx
+								</div>
+							</div>
+
+							<div className="row justify-content-center">
+								<div className="col-4 tab">
+									Bob
+								</div>
+								<div className="col-4" style={{ textAlign: 'left' }}>
+									Total points:xxxx
+								</div>
+							</div>
+
+							<div className="row justify-content-center">
+								<div className="col-4 tab">
+									Rachel
+								</div>
+								<div className="col-4" style={{ textAlign: 'left' }}>
+									Total points:xxxx
+								</div>
+							</div>
+
+							<div className="row justify-content-center">
+								<div className="col-4 tab">
+									Huang
+								</div>
+								<div className="col-4" style={{ textAlign: 'left' }}>
+									Total points:xxxx
+								</div>
+							</div>
                         </div>
                         <div className ="container">
 
-                        
+
                         <button type="button" className="btn btn-secondary" >Remove</button>
                         <button type="button" className="btn btn-primary" style={{ marginLeft: '10px' }}>Add</button>
                         </div>
@@ -29,7 +68,6 @@ class Group extends React.Component {
 					</div>
 					
 				</div>
-			</>
 		);
 	}
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -8,20 +8,38 @@ class profile extends React.Component {
 	render() {
 		return (
 			<>
-				
-
 				<div className="tab-content" id="nav-tabContent">
 					<div className="tab-pane fade show active" id="nav-profile" role="tabpanel"
 						 aria-labelledby="nav-home-tab">
                         <img src={Profilepic} alt="profile" className = "profilepic" />
                         <div className ="settingText">
-                        <text>
-                          Name: xxxxx {"\n"}
-                          </text> <br/>
-                          <text>
-                              Email: xxxxxx{"\n"} 
-                          </text> <br/>
-                          <text>Total points: xxxxx</text> 
+
+							<div className="row justify-content-center">
+								<div className="col-4 tab">
+									Name:
+								</div>
+								<div className="col-4" style={{ textAlign: 'left' }}>
+									xxxxx
+								</div>
+							</div>
+
+							<div className="row justify-content-center">
+								<div className="col-4 tab">
+									Email:
+								</div>
+								<div className="col-4" style={{ textAlign: 'left' }}>
+									xxxxxx
+								</div>
+							</div>
+
+							<div className="row justify-content-center">
+								<div className="col-4 tab">
+									Total points:
+								</div>
+								<div className="col-4" style={{ textAlign: 'left' }}>
+									xxxxx
+								</div>
+							</div>
                         </div>
                           
                     </div>
