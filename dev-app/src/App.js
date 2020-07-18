7645ebed7620b38eea7161b4e48398b72f35e393
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-07-18 15:17:12 +1200

    creating a static modal

--- dev-app/src/App.js
+++ dev-app/src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainApplicationPage from "./components/mainApplicationPage";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-body">
-
-      {/*  /!*<p>*!/*/}
-      {/*  /!*  Edit <code>src/App.js</code> and save to reload.*!/*/}
-      {/*  /!*</p>*!/*/}
-      {/*  /!*<a*!/*/}
-      {/*  /!*  className="App-link"*!/*/}
-      {/*  /!*  href="https://reactjs.org"*!/*/}
-      {/*  /!*  target="_blank"*!/*/}
-      {/*  /!*  rel="noopener noreferrer"*!/*/}
-      {/*  /!*>*!/*/}
-      {/*  /!*  Learn React*!/*/}
-      {/*  /!*</a>*!/*/}
-      {/*  anything*/}
-          <div className="appIcon" >
-              <i className="fas fa-hourglass fa-2x"/>
+      <div className="App">
+          <div className="App-body">
+              <div className="appIcon" >
+                  <i className="fas fa-hourglass fa-2x"/>
+              </div>
           </div>
-      </div>
+
+          <MainApplicationPage />
     </div>
   );
 }
