5db689e8014c11cfb70e67c8205d2427631d7074
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-07-18 21:40:48 +1200

    adding various message popups

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React from 'react';
 import './App.css';
 import MainApplicationPage from "./components/mainApplicationPage";
+import PopupMessage from "./components/PopupMessage";
 
-function App() {
-  return (
-      <div className="App">
-          <div className="App-body">
-              <button type="button" className="appIcon btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                  <i className="fas fa-hourglass fa-2x"/>
-              </button>
-          </div>
+class App extends React.Component {
 
-          <MainApplicationPage />
-    </div>
-  );
+    constructor( props ) {
+        super( props );
+        this.state = {
+            popupType: '0',
+        }
+    }
+
+    showPopup( typeIndex ) {
+
+        if ( typeIndex ) {
+            document.getElementById( 'popupMessageBox' ).style.display = 'block';
+            this.setState({
+                popupType: typeIndex
+            })
+        } else {
+            // dismiss the popup
+            document.getElementById( 'popupMessageBox' ).style.display = 'none';
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-body">
+                    <button type="button" className="appIcon btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        <i className="fas fa-hourglass fa-2x"/>
+                    </button>
+                </div>
+
+                <MainApplicationPage />
+
+                <PopupMessage
+                    type={ this.state.popupType }
+                    changePopup={ ( typeIndex ) => this.showPopup( typeIndex ) }
+                />
+            </div>
+        );
+    }
+
 }
 
 export default App;
