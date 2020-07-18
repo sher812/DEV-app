c2563430e61a38cbf24cafabd662e97a4ace33be
Daniel Li <danielli12357@gmail.com>
2020-07-18 21:25:31 +1200

    Button to change state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import MainApplicationPage from "./components/mainApplicationPage";
+<<<<<<< HEAD
 import PopupMessage from "./components/PopupMessage";
 
 class App extends React.Component {
@@ -46,6 +47,41 @@ class App extends React.Component {
         );
     }
 
+=======
+import './components/timer/timerComplete';
+import TimerComplete from './components/timer/timerComplete';
+
+import './components/timer/timerDigital';
+import TimerDigital from './components/timer/timerDigital';
+import TimeC from './components/timer/timerFormated';
+
+const toggle = ({}) => {
+  console.log("H");
+  return <TimerComplete countdownTime = {6} count = {1} />
+}
+
+function App() {
+  return (
+      <div className="App">
+          <div className="App-body">
+              <button type="button" className="appIcon btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                  <i className="fas fa-hourglass fa-2x"/>
+              </button>
+
+              <TimerComplete countdownTime = {6} count = {0} />
+              {/* <TimerDigital countdownTime = {60}/> */}
+
+              {/* <TimeC /> */}
+
+              <button onClick= {toggle}>
+                <h1> HI </h1>
+              </button>
+          </div>
+
+          <MainApplicationPage />
+    </div>
+  );
+>>>>>>> Button to change state
 }
 
 export default App;
